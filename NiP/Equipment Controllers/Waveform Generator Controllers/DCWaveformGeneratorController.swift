7fd0eef18233db628b5e0f86949a634446e170db
Owen Hildreth <ohildreth@mines.edu>
2021-01-18 23:40:14 -0700

    Started DCWaveformController.

---
+++ NiP/Equipment Controllers/Waveform Generator Controllers/DCWaveformGeneratorController.swift
@@ -0,0 +1,200 @@
+//
+//  DCWaveformGeneratorController.swift
+//  NiP
+//
+//  Created by Owen Hildreth on 1/18/21.
+//
+
+import Foundation
+import Combine
+import SwiftVISA
+
+// TODOP:  Update didSets ton published values.  Create subscribers.
+
+class DCWaveformGeneratorController: EquipmentController, WaveformController {
+    // MARK: - Properties
+    var identifier: String // = "USB0::0x0957::0x2607::MY52200879::INSTR"
+    static var minimumDelay: UInt32 = 2_000_000
+    var voltage = 0.0 {
+        didSet {
+            do {
+                try updateVoltage(voltage)
+            } catch {
+                print("Error when trying to set voltage")
+                print(error) } }
+    }
+    private let startupVoltage = 0.0
+    private let turnedOffVoltage = 0.0
+    private var instrument: MessageBasedInstrument?
+    var outputChannel: UInt
+    var waveformType: WaveformType = .DC {
+        didSet {
+            do {
+                try updateWaveform(waveformType)
+            } catch {
+                print("Error when trying to set Waveform")
+                print(error) }
+        }
+    }
+    
+    
+    var impedence: ImpedenceSetting = .infinite {
+        didSet {
+            do {
+                try updateImpedence(impedence)
+            } catch {
+                print("Error when trying to set Impedence")
+                print(error) }
+        }
+    }
+    
+    
+    // MARK: - Dispatch queue
+    
+    var dispatchQueue: DispatchQueue? {
+        return instrument?.dispatchQueue
+    }
+    
+    
+    // MARK: - Initializers
+    required init(identifier: String, outputChannel: UInt = 1) {
+        
+        print("Initilize instrument")
+
+        self.identifier = identifier
+        
+        self.outputChannel = outputChannel
+        self.waveformType = .DC
+        
+        
+        super.init(equipmentName: "Waveform")
+    }
+    
+    func updateWaveformSettings() {
+        
+    }
+    
+    override func connectToEquipmentController() {
+        
+        guard let newInstrument = try? InstrumentManager.default?.makeInstrument(identifier: identifier) as? MessageBasedInstrument else {
+            print("Could not make waveform generator")
+                return }
+        
+        connectedToController = true
+        equipmentState = .idle
+        
+        self.instrument = newInstrument
+        
+        do {
+            try updateImpedence(self.impedence)
+        } catch { print(error) }
+        
+        do {
+            try updateVoltage(self.voltage)
+        } catch { print(error) }
+        do {
+            try updateWaveform(self.waveformType)
+        } catch { print(error) }
+    }
+}
+
+
+// MARK: - WaveformController Protocol
+extension DCWaveformGeneratorController {
+    func getIdentifier() throws -> String? {
+        return try (instrument?.query("*IDN?\n", as: String.self, decoder: StringDecoder()))
+    }
+    
+    
+    /**
+    Set the Impedence of the instrument
+    
+    - Parameter impedenceSetting: enum of the target impedence
+    */
+    func updateImpedence(_ impedenceSetting: ImpedenceSetting) throws {
+        let outputString = "OUTPUT\(outputChannel)"
+        let impedenceString = impedenceSetting.command()
+        let commandString = outputString + ":" + impedenceString
+        try instrument?.write(commandString)  // Example: OUTPUT1:LOAD INF""
+    }
+    
+    
+    /**
+    Set the Output Voltage of the instrument
+    
+    - Parameter voltage: The target output voltage
+    */
+    func updateVoltage(_ voltage: Double) throws {
+        try instrument?.write("SOURce\(outputChannel):VOLTage:OFFSet \(voltage)")
+    }
+    
+    func updateWaveform(_ waveform: WaveformType) throws {
+        let waveformCommand = waveformType.command()
+        try instrument?.write("SOURce\(outputChannel):\(waveformCommand)")
+    }
+    
+}
+
+// MARK: - Running the Waveform
+extension DCWaveformGeneratorController  {
+    func runWaveform(for runTime: Double) throws {
+        
+        // Rerun setVoltage to make sure the system is definitely on an ready to run the waveform
+        try updateVoltage(voltage)
+        
+        // Turn the channel on.
+        try turnChannelOn()
+        
+        // Make sure that the input runTime isn't negative
+        if runTime < 0 {return}
+        
+        // Calculate the run time
+        let runLength = DispatchTime.now() + Double(runTime)
+        
+        
+        // Have stopWaveform run after runLength
+        
+        DispatchQueue.main.asyncAfter(deadline: runLength, execute: {
+            self.stopWaveform()
+            } as @convention(block) () -> Void)
+
+        
+    }
+    
+    // TODO: Need to make stopWaveform a throwing function.  However, I need to figure out how to do asyn throwing functions
+    func stopWaveform() {
+        do {
+            try instrument?.write("OUTPUT\(outputChannel) OFF")
+        } catch {
+            print(error)
+            self.equipmentState = .notConnected
+        }
+        self.equipmentState = .idle
+    }
+    
+    func turnChannelOn() throws {
+        try instrument?.write("OUTPUT\(outputChannel) ON")
+        self.equipmentState = .busy
+    }
+}
+
+
+// MARK: - Decoders
+extension DCWaveformGeneratorController {
+    private struct StringDecoder: VISADecoder {
+        func decode(_ string: String) throws -> String {
+            var fixedString = string
+            
+            if string.hasPrefix("1`") {
+                fixedString = String(string.dropFirst(2))
+            }
+            
+            while fixedString.hasSuffix("\n") {
+                fixedString = String(fixedString.dropLast())
+            }
+            
+            return fixedString
+        }
+    }
+}
+
--- NiP/Equipment Controllers/Waveform Generator Controllers/WaveformGeneratorController.swift
+++ NiP/Equipment Controllers/Waveform Generator Controllers/WaveformGeneratorController.swift
@@ -9,10 +9,77 @@ import Foundation
 import SwiftVISA
 
 
-class WaveformGeneratorController: EquipmentController {
+protocol WaveformController: class {
+    var outputChannel: UInt { get set }
+    static var minimumDelay: UInt32 { get }
+    var waveformType: WaveformType {get set}
     
+    var dispatchQueue: DispatchQueue? { get }
     
-    init() {
-        
+    init?(identifier: String, outputChannel: UInt) throws
+    
+    func getIdentifier() throws -> String?
+    
+    func updateImpedence(_ impedenceSetting: ImpedenceSetting) throws
+    
+    func updateVoltage(_ voltage: Double) throws
+    
+    func updateWaveform(_ waveform: WaveformType) throws
+    
+    func runWaveform(for runTime: Double) throws
+    
+    func stopWaveform() throws
+}
+
+
+/**
+   Use to set the impedence of the instrument
+   - **standard**: Set the instrument's impedence to a standard 50Ω.  This is often used if you are driving a standard circuit.
+   - **infinite**: Infinite Impedence is used when connecting directly to other high impedence instruments.  If this setting isn't used, then the high impedence instruments will see 2⨉ the desired voltage
+   - **finite(UInt)**: Set impedence to an arbitrary postive value
+   
+    ### Usage Example: ###
+    
+    ````
+    setImpedence(.infinite)
+    
+    setImpedence(.standard)
+    
+    setImpedence(.finite(250))
+     ````
+    
+*/
+enum ImpedenceSetting {
+    /// Set the instrument's impedence to a standard 50Ω.  This is often used if you are driving a standard circuit.
+    case standard
+
+    /// Infinite Impedence is used when connecting directly to other high impedence instruments.  If this setting isn't used, then the high impedence instruments will see 2⨉ the desired volutage
+    case infinite
+
+    /// Set impedence to an arbitrary postive value
+    case finite(UInt)
+    
+    /// Returns a VISA command string that can be used to set the Impedence of the instrument
+    /// - Returns String: The VISA command to set the Impedence
+    func command() -> String {
+        switch self {
+        case .standard:
+            return "LOAD \(50)"
+        case .infinite:
+            return "LOAD INF"
+        case let .finite(value):
+            return "LOAD \(value)"
+        }
+    }
+}
+
+enum WaveformType {
+    case DC
+    
+    func command() -> String {
+        switch self {
+        case .DC:
+            return "FUNCtion DC"
+        }
     }
 }
